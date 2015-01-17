a14e90bf40c387b8930e15771d3ce3e94bd4dc99
Jonnie Spratley <jonniespratley@gmail.com>
2015-01-16 22:46:16 -0800

    added stackato file

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express');
-var cfenv = require('cfenv');
+//var cfenv = require('cfenv');
+
+var port = process.env.PORT || 1337;
+var host = process.env.VCAP_APP_HOST || "127.0.0.1";
 var app = express();
-var appEnv = cfenv.getAppEnv();
-var DIST_ROOT = __dirname + '/dist'
+//var appEnv = cfenv.getAppEnv();
+var DIST_ROOT = __dirname + '/dist';
 var BOWER_ROOT = __dirname + '/';
 var APP_ROOT = __dirname + '/app';
 
-app.set('port', appEnv.port);
-app.set('host', appEnv.bind);
- 
+app.set('port', port);
+app.set('host', host);
+
+app.use(express.static(APP_ROOT));
 app.use(express.static(DIST_ROOT));
 
 app.get('/', function(request, response) {
@@ -16,5 +20,24 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), app.get('host'), function() {
-	console.log('Node app is running at:' + appEnv.url);
+	console.log('Node app is running at:' + host + ':' + port);
 });
+
+
+if(process.env.VCAP_SERVICES){
+  var services = JSON.parse(process.env.VCAP_SERVICES);
+  var dbcreds = services['mongodb'][0].credentials;
+}
+
+if(dbcreds){
+  console.log(dbcreds);
+  mongoose.connect(dbcreds.host, dbcreds.db, dbcreds.port, {user: dbcreds.username, pass: dbcreds.password});
+} else {
+  //mongoose.connect("127.0.0.1", "myappdb", 27017);
+}
+console.log("attempting to connect to mongodb");
+if(process.env.MONGODB_URL){
+  mongoose.connect(process.env.MONGODB_URL);
+} else {
+//  mongoose.connect("127.0.0.1", "myappdb", 27017);
+}
